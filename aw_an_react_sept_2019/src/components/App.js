878eddaea60ab32cbb3a5c9e80d5c302394ce5b8
Hano <hinsul3113@gmail.com>
2019-11-12 11:30:49 -0800

    Add demo for Dynamic QuestionDetails

--- aw_an_react_sept_2019/src/components/App.js
+++ aw_an_react_sept_2019/src/components/App.js
@@ -16,12 +16,57 @@ function App() {
 // that simulate the pages of web application. These are
 // meant to replace the various pages rendered by the routes
 // of our rails server.
+
 function QuestionShowPage() {
   return (
     <main>
-      <QuestionDetails />
+      <QuestionDetails
+        title="What is your favourite color?"
+        body="Red, green, blue, magenta etc."
+        author={{ full_name: "David Ko" }}
+        view_count={100}
+        create_at={new Date().toLocaleString()}
+      />
       <AnswerDetails />
     </main>
+  );
+}
+
+//  A react component is a function that returns a React element.
+// React elements are created with the 'React.createElement()' method
+// or JSX tags.
+
+// Your React component's names must be in PascalCase. Those
+// whose names do not begin with an upper case letter will
+// be interpreted as plain HTML tag.
+
+function QuestionDetails(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>
+        {props.body}
+        <br />
+        By {props.author && props.author.full_name}
+      </p>
+      <p>
+        <small>
+          Seen {props.view_count} time(s) - Created at {props.create_at}
+        </small>
+      </p>
+    </div>
+  );
+}
+
+function AnswerDetails() {
+  return (
+    <div>
+      <p>
+        Red. <br />
+        By Gurshant <br />
+        <small>Answered 2 days ago</small>
+      </p>
+    </div>
   );
 }
 
