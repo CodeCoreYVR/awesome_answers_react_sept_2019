cfd28fdf6213e05c981d72434f31c693df5b3ab6
Hano <hinsul3113@gmail.com>
2019-11-12 10:31:11 -0800

    Initial commit from react react app

---
+++ aw_an_react_sept_2019/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
