352e165c2b01932be7a652d82238197a3eaf713d
Hano <hinsul3113@gmail.com>
2019-11-13 10:13:29 -0800

    Add demo for using state

--- aw_an_react_sept_2019/src/components/QuestionShowPage.js
+++ aw_an_react_sept_2019/src/components/QuestionShowPage.js
@@ -5,16 +5,20 @@ import { AnswerList } from "./AnswerList";
 import questionData from "../questionData";
 
 class QuestionShowPage extends Component {
+  constructor(props) {
+    // When using a constructor in a class-based
+    // component, you must call the 'Component' class
+    // constructor with 'super' passing it props
+    super(props);
+
+    this.state = {
+      question: questionData[0]
+    };
+  }
   render() {
     return (
       <main>
-        <QuestionDetails
-          title="What is your favourite color?"
-          body="Red, green, blue, magenta etc."
-          author={{ full_name: "David Ko" }}
-          view_count={100}
-          created_at={new Date().toLocaleString()}
-        />
+        <QuestionDetails {...this.state.question} />
         <h2>Answers</h2>
         <AnswerList answers={questionData[0].answers} />
       </main>
