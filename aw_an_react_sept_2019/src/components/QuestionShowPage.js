436a7c90cce4e98b569e52cb6689c3f133730685
Hano <hinsul3113@gmail.com>
2019-11-13 09:58:30 -0800

    Add demo for Question Show goes to Class

--- aw_an_react_sept_2019/src/components/QuestionShowPage.js
+++ aw_an_react_sept_2019/src/components/QuestionShowPage.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
 import QuestionDetails from "./QuestionDetails";
 import { AnswerList } from "./AnswerList";
 import questionData from "../questionData";
 
-function QuestionShowPage() {
-  return (
-    <main>
-      <QuestionDetails
-        title="What is your favourite color?"
-        body="Red, green, blue, magenta etc."
-        author={{ full_name: "David Ko" }}
-        view_count={100}
-        created_at={new Date().toLocaleString()}
-      />
-      <h2>Answers</h2>
-      <AnswerList answers={questionData[0].answers} />
-    </main>
-  );
+class QuestionShowPage extends Component {
+  render() {
+    return (
+      <main>
+        <QuestionDetails
+          title="What is your favourite color?"
+          body="Red, green, blue, magenta etc."
+          author={{ full_name: "David Ko" }}
+          view_count={100}
+          created_at={new Date().toLocaleString()}
+        />
+        <h2>Answers</h2>
+        <AnswerList answers={questionData[0].answers} />
+      </main>
+    );
+  }
 }
 
 export default QuestionShowPage;
