db35db67cbb4332385832ab6445fc6059f5b998f
Hano <hinsul3113@gmail.com>
2019-11-13 10:01:15 -0800

    Add demo for QuestionIndexPage goes to class

--- aw_an_react_sept_2019/src/components/QuestionIndexPage.js
+++ aw_an_react_sept_2019/src/components/QuestionIndexPage.js
@@ -2,22 +2,24 @@ import React from "react";
 
 import data from "../questionData";
 
-export function QuestionIndexPage(props) {
-  return (
-    <main className="QuestionIndexPage">
-      <h2>Questions</h2>
-      <ul
-        style={{
-          listStyle: "none",
-          paddingLeft: 0
-        }}
-      >
-        {data.map(question => (
-          <li key={question.id}>
-            <a href="">{question.title}</a>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+export class QuestionIndexPage extends React.Component {
+  render() {
+    return (
+      <main className="QuestionIndexPage">
+        <h2>Questions</h2>
+        <ul
+          style={{
+            listStyle: "none",
+            paddingLeft: 0
+          }}
+        >
+          {data.map(question => (
+            <li key={question.id}>
+              <a href="">{question.title}</a>
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+  }
 }
