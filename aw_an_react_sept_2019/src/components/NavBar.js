cbb719079bdb5d034caf657958d850ebb18d1f6e
Hano <hinsul3113@gmail.com>
2019-11-19 10:41:45 -0800

    Update currentUser state in App on App mounts and on Sign in

--- aw_an_react_sept_2019/src/components/NavBar.js
+++ aw_an_react_sept_2019/src/components/NavBar.js
@@ -3,11 +3,14 @@ import { NavLink /* Link */ } from "react-router-dom";
 import CurrentDateTime from "./CurrentDateTime";
 
 function NavBar(props) {
+  const { currentUser } = props;
   return (
     <div className="ui pointing menu">
       <NavLink to="/questions" className="item">
         Questions
       </NavLink>
+      {!currentUser && <NavLink to="sign_in">Sign In </NavLink>}
+      {currentUser && <span>Welcome {currentUser.full_name}</span>}
       <div className="right menu">
         <CurrentDateTime />
       </div>
